613da335e8a0eae192e813b6956b8d1ef3bf96a1
Beignet <nterrien0@gmail.com>
2025-08-13 12:11:18 +0200

    Fixed abilities

--- abilities.js
+++ abilities.js
@@ -9,12 +9,12 @@ abilities = {
     "Motor Drive": { "Electric": 0 },
     "Purifying Salt": { "Ghost": 0.5 },
     "Sap Sipper": { "Grass": 0 },
-    "Storm Drain ": { "Water": 0 },
+    "Storm Drain": { "Water": 0 },
     "Thick Fat": { "Fire": 0.5, "Ice": 0.5 },
-    "Volt Absorb ": { "Electric": 0 },
+    "Volt Absorb": { "Electric": 0 },
     "Water Absorb": { "Water": 0 },
     "Water Bubble": { "Fire": 0.5 },
-    "Well-Baked Bod:": { "Fire": 0 },
+    "Well-Baked Body": { "Fire": 0 },
     // It doesn't really works like that but it works if Shedinja type is unchanged and if it is the only one with this ability
     "Wonder Guard": { "Normal": 0, "Fighting": 0, "Poison": 0, "Ground": 0, "Bug": 0, "Steel": 0, "Water": 0, "Grass": 0, "Electric": 0, "Psychic": 0, "Ice": 0, "Dragon": 0, "Fairy": 0 },
 }
